abb0de39e328c310590f9f2b24997026a797aa63
Pavucs Manokar <smpavucsan@gmail.com>
2022-12-04 23:33:35 +0530

    auth configuration added

---
+++ src/main/java/com/ilabs/authservice/configuration/WebConfiguration.java
@@ -0,0 +1,6 @@
+package com.ilabs.authservice.configuration;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class WebConfiguration extends WebSecurityConfigurerAdapter {
+}
